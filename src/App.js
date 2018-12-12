2080098d4664aa773a0b6f23c4cbab83ebb32891
Bayon Forte <bayon@forteworks.com>
2018-12-12 07:50:32 -0500

    moved posts into component class

--- src/App.js
+++ src/App.js
@@ -5,38 +5,15 @@ import Post from './components/themes/theme-one/Post'
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      posts: []
-    }
   }
-  componentDidMount() {
-    let postsURL = "http://localhost:8888/wp/wp-json/wp/v2/posts";
-    fetch(postsURL)
-    .then(response => response.json())
-    .then( response => {
-         console.log(response);
-          this.setState({
-            posts: response
-          });
-          
-    })
-}
+  
 render() {
-  let posts = this.state.posts.map((post,index) => {
-      return(
-          <div key={index} className="post">
-          <h2>{post.title.rendered} </h2>
-          
-          <p dangerouslySetInnerHTML={{__html: post.content.rendered}} ></p>
-          <p>{post.date} </p>
-          </div>
-      )
-  })
+  
   return (
       <div className="App">
-          <h1>Posts</h1>
-          <h3>In React from Wordpress Backend.</h3>
-          {posts}
+          <h1>WP</h1>
+          <h3>React Frontend with Wordpress Backend.</h3>
+           <Post  ></Post>
           <hr/>
           <p>to do:</p>
             <ul>
--- src/components/themes/theme-one/Post.js
+++ src/components/themes/theme-one/Post.js
@@ -2,10 +2,51 @@ import React, { Component } from 'react';
 import './Post.css';
 
 class Post extends Component {
-    constructor() {
-        console.log(this.state.posts);
+    constructor(props) {
+         
+        super(props);
+        this.state = {
+          posts: [],
+          configs:{
+              apiURL:"http://localhost:8888/wp/wp-json/wp/v2/"
+          }
+        }
       }
-    
+      componentDidMount() {
+        let postsURL = this.state.configs.apiURL+"posts";
+        fetch(postsURL)
+        .then(response => response.json())
+        .then( response => {
+             console.log(response);
+              this.setState({
+                posts: response
+              });
+              
+        })
+    }
+
+    render () {
+        let posts = this.state.posts.map((post,index) => {
+            return(
+                <div key={index} className="post">
+                <h2>{post.title.rendered} </h2>
+                <p dangerouslySetInnerHTML={{__html: post.content.rendered}} ></p>
+                <p>{post.date} </p>
+                </div>
+            )
+        })
+
+        return (
+            <div className="posts">
+            <h2>Posts:</h2>
+             {posts}
+              
+            </div>
+           
+             
+        )
+    }
+
     
     
 }
